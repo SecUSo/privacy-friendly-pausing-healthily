101c31fc493d30f16f55fcf57192b55a76d768bc
Jannik <badric@hotmail.de>
2016-08-24 13:34:56 +0200

    help
    Added help

--- app/src/main/java/orgprivacy_friendly_apps/secuso/privacyfriendlybreakreminder/HelpActivity.java
+++ app/src/main/java/orgprivacy_friendly_apps/secuso/privacyfriendlybreakreminder/HelpActivity.java
@@ -1,9 +1,17 @@
 package orgprivacy_friendly_apps.secuso.privacyfriendlybreakreminder;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -12,6 +20,7 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
         setupActionBar();
+
     }
 
     private void setupActionBar() {
@@ -33,4 +42,45 @@ public class HelpActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void clickHandler(View v){
+        switch (v.getId()){
+            case R.id.imageView:
+                setContentView(R.layout.help2);
+                ImageView img = (ImageView) findViewById(R.id.img_back);
+                img.setImageResource(R.drawable.start_screen);
+                break;
+            case R.id.imageView1:
+                setContentView(R.layout.help2);
+                img = (ImageView) findViewById(R.id.img_back);
+                img.setImageResource(R.drawable.settings_screen);
+                break;
+            case R.id.imageView2:
+                setContentView(R.layout.help2);
+                img = (ImageView) findViewById(R.id.img_back);
+                img.setImageResource(R.drawable.create_profile_screen);
+                break;
+            case R.id.imageView3:
+                setContentView(R.layout.help2);
+                img = (ImageView) findViewById(R.id.img_back);
+                img.setImageResource(R.drawable.create_profile_screen);
+                break;
+            case R.id.imageView4:
+                setContentView(R.layout.help2);
+                img = (ImageView) findViewById(R.id.img_back);
+                img.setImageResource(R.drawable.general_screen);
+                break;
+            case R.id.imageView5:
+                setContentView(R.layout.help2);
+                img = (ImageView) findViewById(R.id.img_back);
+                img.setImageResource(R.drawable.notification_screen);
+                break;
+            case R.id.img_back:
+                setContentView(R.layout.help);
+                break;
+            default:
+
+        }
+    }
+
 }

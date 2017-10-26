cc83286330eccfca4e300bde383150d866ad6071
Christopher Beckmann <chris.beckm@gmail.com>
2017-10-26 13:22:25 +0200

    Cleanup of old code and Bugfixing.

--- app/src/main/java/org/secuso/privacyfriendlybreakreminder/activities/adapter/ExerciseSetListAdapter.java
+++ app/src/main/java/org/secuso/privacyfriendlybreakreminder/activities/adapter/ExerciseSetListAdapter.java
@@ -61,6 +61,7 @@ public class ExerciseSetListAdapter extends RecyclerView.Adapter<RecyclerView.Vi
 
     public void disableDeleteMode() {
         deleteMode = false;
+        deleteIds.clear();
         notifyDataSetChanged();
     }
 

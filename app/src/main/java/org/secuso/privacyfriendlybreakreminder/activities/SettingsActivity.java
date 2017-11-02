fda52b318d7163f9c748f0fd790162368ee1ee0f
Christopher Beckmann <chris.beckm@gmail.com>
2017-11-02 11:22:56 +0100

    Reset Settings Activity to not show the NavigationDrawer. Added a TimePreference for the scheduler. Also added the Continuous Setting.

--- app/src/main/java/org/secuso/privacyfriendlybreakreminder/activities/SettingsActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlybreakreminder/activities/SettingsActivity.java
@@ -3,72 +3,32 @@ package org.secuso.privacyfriendlybreakreminder.activities;
 
 
 import android.annotation.TargetApi;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.StringRes;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.TaskStackBuilder;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import org.secuso.privacyfriendlybreakreminder.R;
 import org.secuso.privacyfriendlybreakreminder.activities.helper.AppCompatPreferenceActivity;
-import org.secuso.privacyfriendlybreakreminder.activities.tutorial.TutorialActivity;
 
 import java.util.List;
-
-import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
 
 /**
  * @author Christopher Beckmann
  * @version 2.0
  */
-public class SettingsActivity extends AppCompatPreferenceActivity implements NavigationView.OnNavigationItemSelectedListener{
-
-    // delay to launch nav drawer item, to allow close animation to play
-    protected static final int NAVDRAWER_LAUNCH_DELAY = 250;
-    // fade in and fade out durations for the main content when switching between
-    // different Activities of the app through the Nav Drawer
-    protected static final int MAIN_CONTENT_FADEOUT_DURATION = 150;
-    protected static final int MAIN_CONTENT_FADEIN_DURATION = 250;
-
-    // Navigation drawer:
-    protected DrawerLayout mDrawerLayout;
-    private NavigationView mNavigationView;
-    protected Toolbar toolbar;
-    protected ActionBarDrawerToggle mDrawerToggle;
+public class SettingsActivity extends AppCompatPreferenceActivity {
 
     // Helper
     private Handler mHandler;
     protected SharedPreferences mSharedPreferences;
-
-    private boolean mDrawerEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,15 +38,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Nav
         mHandler = new Handler();
 
         overridePendingTransition(0, 0);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mDrawerLayout != null && mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-            mDrawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     /**
@@ -116,122 +67,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Nav
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return goToNavigationItem(item.getItemId());
-    }
-
-    protected boolean goToNavigationItem(final int itemId) {
-
-        if(itemId == getNavigationDrawerID()) {
-            // just close drawer because we are already in this activity
-            mDrawerLayout.closeDrawer(GravityCompat.START);
-            return true;
-        }
-
-        // delay transition so the drawer can close
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                callDrawerItem(itemId);
-            }
-        }, NAVDRAWER_LAUNCH_DELAY);
-
-        mDrawerLayout.closeDrawer(GravityCompat.START);
-
-        selectNavigationItem(itemId);
-
-        // fade out the active activity
-        View mainContent = findViewById(R.id.main_content);
-        if (mainContent != null) {
-            mainContent.animate().alpha(0).setDuration(MAIN_CONTENT_FADEOUT_DURATION);
-        }
-        return true;
-    }
-
-    // set active navigation item
-    private void selectNavigationItem(int itemId) {
-        for(int i = 0 ; i < mNavigationView.getMenu().size(); i++) {
-            boolean b = itemId == mNavigationView.getMenu().getItem(i).getItemId();
-            mNavigationView.getMenu().getItem(i).setChecked(b);
-        }
-    }
-
-    /**
-     * Enables back navigation for activities that are launched from the NavBar. See
-     * {@code AndroidManifest.xml} to find out the parent activity names for each activity.
-     * @param intent
-     */
-    private void createBackStack(Intent intent) {
-        TaskStackBuilder builder = TaskStackBuilder.create(this);
-        builder.addNextIntentWithParentStack(intent);
-        builder.startActivities();
-    }
-
-    /**
-     * This method manages the behaviour of the navigation drawer
-     * Add your menu items (ids) to res/menu/activity_main_drawer.xml
-     * @param itemId Item that has been clicked by the user
-     */
-    private void callDrawerItem(final int itemId) {
-
-        Intent intent;
-
-        switch(itemId) {
-            case R.id.nav_timer:
-                intent = new Intent(this, TimerActivity.class);
-                intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                break;
-            case R.id.nav_manage_exercise_sets:
-                intent = new Intent(this, ManageExerciseSetsActivity.class);
-                createBackStack(intent);
-                break;
-            case R.id.nav_tutorial:
-                intent = new Intent(this, TutorialActivity.class);
-                createBackStack(intent);
-                break;
-            case R.id.nav_about:
-                intent = new Intent(this, AboutActivity.class);
-                createBackStack(intent);
-                break;
-            case R.id.nav_help:
-                intent = new Intent(this, HelpActivity.class);
-                createBackStack(intent);
-                break;
-            case R.id.nav_settings:
-                intent = new Intent(this, SettingsActivity.class);
-                //intent.putExtra( PreferenceActivity.EXTRA_SHOW_FRAGMENT, SettingsActivity.ExercisePreferenceFragment.class.getName() );
-                //intent.putExtra( PreferenceActivity.EXTRA_NO_HEADERS, true );
-                createBackStack(intent);
-                break;
-            default:
-        }
-        overridePendingTransition(0,0);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_settings);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if(getSupportActionBar() == null) {
-            setSupportActionBar(toolbar);
-        }
-
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        mDrawerLayout.addDrawerListener(mDrawerToggle);
-//        mDrawerToggle.syncState();
-//
-//        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
-//        mNavigationView.setNavigationItemSelectedListener(this);
-//
-//        showContent();
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -242,95 +77,12 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Nav
         }
     }
 
-    public void setDrawerEnabled(final boolean enabled) {
-
-        int lockMode = enabled ?
-                DrawerLayout.LOCK_MODE_UNLOCKED :
-                DrawerLayout.LOCK_MODE_LOCKED_CLOSED;
-
-        mDrawerEnabled = enabled;
-
-        if(mDrawerLayout == null) return;
-
-        mDrawerLayout.setDrawerLockMode(lockMode);
-        mDrawerToggle.setDrawerIndicatorEnabled(enabled);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(!enabled);
-            actionBar.setDisplayShowHomeEnabled(enabled);
-            actionBar.setHomeButtonEnabled(enabled);
-        }
-
-        mDrawerToggle.syncState();
-    }
-
     @Override
     protected boolean isValidFragment(String fragmentName) {
         return ExercisePreferenceFragment.class.getName().equals(fragmentName)
                 || TimerPreferenceFragment.class.getName().equals(fragmentName);
     }
 
-    private void showContent() {
-        selectNavigationItem(getNavigationDrawerID());
-
-        View mainContent = findViewById(R.id.main_content);
-        if (mainContent != null) {
-            mainContent.setAlpha(0);
-            mainContent.animate().alpha(1).setDuration(MAIN_CONTENT_FADEIN_DURATION);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        //showContent();
-    }
-
-    /**
-     * Start a new fragment.
-     *
-     * @param fragment The fragment to start
-     * @param push If true, the current fragment will be pushed onto the back stack.  If false,
-     * the current fragment will be replaced.
-     */
-    @Override
-    public void startPreferenceFragment(Fragment fragment, boolean push) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.prefs, fragment);
-        if (push) {
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            transaction.addToBackStack(":android:prefs");
-        } else {
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        }
-        transaction.commitAllowingStateLoss();
-    }
-
-    /**
-     * Start a new fragment containing a preference panel.  If the preferences
-     * are being displayed in multi-pane mode, the given fragment class will
-     * be instantiated and placed in the appropriate pane.  If running in
-     * single-pane mode, a new activity will be launched in which to show the
-     * fragment.
-     *
-     * @param fragmentClass Full name of the class implementing the fragment.
-     * @param args Any desired arguments to supply to the fragment.
-     * @param titleRes Optional resource identifier of the title of this
-     * fragment.
-     * @param titleText Optional text of the title of this fragment.
-     * @param resultTo Optional fragment that result data should be sent to.
-     * If non-null, resultTo.onActivityResult() will be called when this
-     * preference panel is done.  The launched panel must use
-     * {@link #finishPreferencePanel(Fragment, int, Intent)} when done.
-     * @param resultRequestCode If resultTo is non-null, this is the caller's
-     * request code to be received with the result.
-     */
-    public void startPreferencePanel(String fragmentClass, Bundle args, @StringRes int titleRes,
-                                     CharSequence titleText, Fragment resultTo, int resultRequestCode) {
-        startWithFragment(fragmentClass, args, resultTo, resultRequestCode, titleRes, 0);
-    }
 
     /**
      * A preference value change listener that updates the preference's summary
@@ -392,7 +144,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Nav
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_general);
+            addPreferencesFromResource(R.xml.pref_exercise);
             setHasOptionsMenu(true);
 
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
@@ -401,25 +153,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Nav
             // guidelines.
             bindPreferenceSummaryToValue(findPreference("pref_exercise_time"));
         }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-
-            SettingsActivity settingsActivity = (SettingsActivity)getActivity();
-            if(settingsActivity != null) settingsActivity.setDrawerEnabled(false);
-        }
-
-//        @Override
-//        public boolean onOptionsItemSelected(MenuItem item) {
-//            int id = item.getItemId();
-//            if (id == android.R.id.home) {
-//                Log.d("Fragment", "clicked");
-//                onBackPressed();
-//                return true;
-//            }
-//            return super.onOptionsItemSelected(item);
-//        }
     }
 
     public static class TimerPreferenceFragment extends PreferenceFragment {
@@ -429,22 +162,5 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Nav
             addPreferencesFromResource(R.xml.pref_scheduler);
             setHasOptionsMenu(true);
         }
-
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            Log.d("WTF", "destroy?");
-        }
-
-        //        @Override
-//        public boolean onOptionsItemSelected(MenuItem item) {
-//            int id = item.getItemId();
-//            if (id == android.R.id.home) {
-//              //startActivity(new Intent(getActivity(), SettingsActivity.class));
-//                return true;
-//            }
-//            return super.onOptionsItemSelected(item);
-//        }
-
     }
 }

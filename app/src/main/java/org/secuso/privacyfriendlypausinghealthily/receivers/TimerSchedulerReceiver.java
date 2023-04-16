4d3b910f4f62a2d9f04d4f2bcab06f039f1149de
Patrick Schneider <urulu@student.kit.edu>
2023-04-16 19:26:55 +0200

    Fixes some errors due to dependency updates

--- app/src/main/java/org/secuso/privacyfriendlypausinghealthily/receivers/TimerSchedulerReceiver.java
+++ app/src/main/java/org/secuso/privacyfriendlypausinghealthily/receivers/TimerSchedulerReceiver.java
@@ -79,7 +79,7 @@ public class TimerSchedulerReceiver extends WakefulBroadcastReceiver {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
         Intent automaticTimerIntent = new Intent(context, TimerSchedulerReceiver.class);
-        PendingIntent automaticTimerPending = PendingIntent.getBroadcast(context, 0, automaticTimerIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent automaticTimerPending = PendingIntent.getBroadcast(context, 0, automaticTimerIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(timestamp);
@@ -156,7 +156,7 @@ public class TimerSchedulerReceiver extends WakefulBroadcastReceiver {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
         Intent automaticTimerIntent = new Intent(context, TimerSchedulerReceiver.class);
-        PendingIntent automaticTimerPending = PendingIntent.getBroadcast(context, 0, automaticTimerIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent automaticTimerPending = PendingIntent.getBroadcast(context, 0, automaticTimerIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
         alarmManager.cancel(automaticTimerPending);
     }
